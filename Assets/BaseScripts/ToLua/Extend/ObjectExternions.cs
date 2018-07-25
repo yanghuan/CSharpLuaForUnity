8892ba8cae753e45146d6bb646adf6e0da91f4b6
jinjun.ljj <jinjun.ljj@alibaba-inc.com>
2018-07-25 22:06:34 +0800

    实现UnityEngine.Object的导出

---
+++ Assets/BaseScripts/ToLua/Extend/ObjectExternions.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using LuaInterface;
+
+namespace Generic
+{
+    public static class ObjectExternions
+    {
+        public static Object Instantiate1(Object original, System.Type T)
+        {
+            return Object.Instantiate(original);
+        }
+
+        public static Object Instantiate3(Object original, UnityEngine.Transform parent, System.Type T)
+        {
+            return Object.Instantiate(original, parent);
+        }
+
+        public static Object Instantiate4(Object original, UnityEngine.Transform parent, System.Boolean worldPositionStays, System.Type T)
+        {
+            return Object.Instantiate(original, parent, worldPositionStays);
+        }
+
+        public static Object Instantiate5(Object original, UnityEngine.Vector3 position, UnityEngine.Quaternion rotation, System.Type T)
+        {
+            return Object.Instantiate(original, position, rotation);
+        }
+
+        public static Object Instantiate9(Object original, UnityEngine.Vector3 position, UnityEngine.Quaternion rotation, UnityEngine.Transform parent, System.Type T)
+        {
+            return Object.Instantiate(original, position, rotation, parent);
+        }
+
+        public static Object FindObjectOfType0T(System.Type T)
+        {
+            return Object.FindObjectOfType(T);
+        }
+        public static Object[] FindObjectsOfType0T(System.Type T)
+        {
+            return Object.FindObjectsOfType(T);
+        }
+    }
+}
