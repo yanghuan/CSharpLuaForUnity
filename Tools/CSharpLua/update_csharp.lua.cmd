6dc87085b5f4cf4aa160807073daf24161f9dd22
yanghuan <sy.yanghuan@gmail.com>
2018-12-27 10:47:28 +0800

    更新CSharp.lua编译器

--- Tools/CSharpLua/update_csharp.lua.cmd
+++ Tools/CSharpLua/update_csharp.lua.cmd
@@ -1,7 +1,7 @@
 set csharplua=D:\Project\Person\CSharp.lua
 set launcher=%csharplua%\CSharp.lua.Launcher\bin\Debug\netcoreapp2.0
 set coresystem=%csharplua%\CSharp.lua\CoreSystem.Lua
-set localcoresystem=..\Assets\Lua\CoreSystemLua
+set localcoresystem=..\..\Assets\Lua\CoreSystemLua
 
 xcopy %launcher% CSharp.lua /y 
 del /s /q %localcoresystem%\*.lua
