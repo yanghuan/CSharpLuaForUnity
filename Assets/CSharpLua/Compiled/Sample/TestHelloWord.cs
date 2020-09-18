b40a26bda750649ad056a06c9919042291581a2a
yanghuan <sy.yanghuan@gmail.com>
2020-09-18 15:24:22 +0800

    1.更新tolua 1.0.8.595 处理unity运行库[out]标记非out参数问题 2.System.typeof 兼容适配

--- Assets/CSharpLua/Compiled/Sample/TestHelloWord.cs
+++ Assets/CSharpLua/Compiled/Sample/TestHelloWord.cs
@@ -2,9 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using UnityEditor;
 using UnityEngine;
 
 namespace Sample {
